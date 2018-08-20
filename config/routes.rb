03a829fdf8fdb35f8eff3e2a36043fe317edd93e
DavidRZuluaga <git@davidrzuluaga.com>
2018-08-19 21:53:46 -0500

    ejercicio 4.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get '/', to: 'actors#index'
+  get '/actors', to: 'actors#index'
   get 'actors/new', to: 'actors#new', as: 'new_actor'
   post '/actors', to: 'actors#create'
 
