2a92bc1b76d16ebe29a4a33d5bff781abe2b4ba1
DavidRZuluaga <git@davidrzuluaga.com>
2018-08-19 23:00:19 -0500

    ejercicio.1

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -9,12 +9,16 @@ class ActorsController < ApplicationController
     end
 
     def create
-        actor = Actor.new(params[:actor])
+        actor = Actor.new(artist_params)
         if actor.save
             redirect_to actors_path
         else
             render :new
         end
+    end
+      
+    def artist_params
+        params.require(:actor).permit(:death_place, :death_date, :image_url, :birth_place, :birth_date, :bio, :name)
     end
 
 end
